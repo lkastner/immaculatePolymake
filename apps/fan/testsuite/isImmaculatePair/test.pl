444024576a36aceff5b9d24fbc6bd5f7191ae606
Lars Kastner <lars.kastner@yahoo.dk>
2017-03-02 20:54:11 +0100

    Fixing order in compare_values

--- apps/fan/testsuite/isImmaculatePair/test.pl
+++ apps/fan/testsuite/isImmaculatePair/test.pl
@@ -4,9 +4,9 @@ my $simplex = simplex(2);
 my $simplexTwo = simplex(2,2);
 my $simplexThree = simplex(2,3);
 my $pt = new Polytope(POINTS=>[[1,0,0]]);
-compare_values("(cube,pt)", isImmaculatePair($cube, $pt), True);
-compare_values("(cubeTwo,pt)", isImmaculatePair($cubeTwo, $pt), False);
-compare_values("(simplex,pt)", isImmaculatePair($simplex, $pt), True);
-compare_values("(simplexTwo,pt)", isImmaculatePair($simplexTwo, $pt), True);
-compare_values("(simplexThree,pt)", isImmaculatePair($simplexThree, $pt), False);
+compare_values("(cube,pt)", True, isImmaculatePair($cube, $pt));
+compare_values("(cubeTwo,pt)", False, isImmaculatePair($cubeTwo, $pt));
+compare_values("(simplex,pt)", True, isImmaculatePair($simplex, $pt));
+compare_values("(simplexTwo,pt)", True, isImmaculatePair($simplexTwo, $pt));
+compare_values("(simplexThree,pt)", False, isImmaculatePair($simplexThree, $pt));
 
