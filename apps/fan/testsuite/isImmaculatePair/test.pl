195849cc982813822f1f4eab473184149f4f5558
Lars Kastner <lars.kastner@yahoo.dk>
2017-03-02 22:09:35 +0100

    Fixed problem, using 0 and 1 instead of pm bools

--- apps/fan/testsuite/isImmaculatePair/test.pl
+++ apps/fan/testsuite/isImmaculatePair/test.pl
@@ -1,12 +1,12 @@
-my $cube = cube(2);
+my $cube = cube(2,1,0);
 my $cubeTwo = cube(2,2,0);
 my $simplex = simplex(2);
 my $simplexTwo = simplex(2,2);
 my $simplexThree = simplex(2,3);
 my $pt = new Polytope(POINTS=>[[1,0,0]]);
-compare_values("(cube,pt)", True, isImmaculatePair($cube, $pt));
-compare_values("(cubeTwo,pt)", False, isImmaculatePair($cubeTwo, $pt));
-compare_values("(simplex,pt)", True, isImmaculatePair($simplex, $pt));
-compare_values("(simplexTwo,pt)", True, isImmaculatePair($simplexTwo, $pt));
-compare_values("(simplexThree,pt)", False, isImmaculatePair($simplexThree, $pt));
+compare_values("(cube,pt)", 1, isImmaculatePair($cube, $pt));
+compare_values("(cubeTwo,pt)", 0, isImmaculatePair($cubeTwo, $pt));
+compare_values("(simplex,pt)", 1, isImmaculatePair($simplex, $pt));
+compare_values("(simplexTwo,pt)", 1, isImmaculatePair($simplexTwo, $pt));
+compare_values("(simplexThree,pt)", 0, isImmaculatePair($simplexThree, $pt));
 
