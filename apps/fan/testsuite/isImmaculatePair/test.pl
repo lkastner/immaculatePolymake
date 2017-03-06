660098af78b232efd1b16f07f3e1640257c09a7d
Lars Kastner <lars.kastner@yahoo.dk>
2017-03-06 15:41:50 +0100

    New user_function for computing immaculacyMatrix, some additions to tex about algorithms

--- apps/fan/testsuite/isImmaculatePair/test.pl
+++ apps/fan/testsuite/isImmaculatePair/test.pl
@@ -10,3 +10,10 @@ compare_values("(simplex,pt)", 1, isImmaculatePair($simplex, $pt));
 compare_values("(simplexTwo,pt)", 1, isImmaculatePair($simplexTwo, $pt));
 compare_values("(simplexThree,pt)", 0, isImmaculatePair($simplexThree, $pt));
 
+my $line = new Polytope(POINTS=>[[1,0,0],[1,0,2]]);
+compare_values("(line, simplex)", 0, isImmaculatePair($line, $simplex));
+compare_values("(simplex, line)", 1, isImmaculatePair($simplex, $line));
+
+my $diagonal = new Polytope(POINTS=>[[1,0,0],[1,1,1]]);
+compare_values("(simplex, diagonal)", 0, isImmaculatePair($simplex, $diagonal));
+compare_values("(diagonal, simplex)", 1, isImmaculatePair($diagonal, $simplex));
