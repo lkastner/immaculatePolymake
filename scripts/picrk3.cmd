7f3c3d381c5ed4031a6f7b2a10b57ec0d792a59b
Lars Kastner <lars.kastner@yahoo.dk>
2017-06-16 10:20:19 +0200

    More code + output on pic rk 3

--- scripts/picrk3.cmd
+++ scripts/picrk3.cmd
@@ -90,6 +90,8 @@ foreach my $line (@lines){
    @lpts = grep(!$line->contains($_), @lpts);
 }
 print new Matrix(@lpts);
+
+# Fourth example
 $s1 = new Polytope(POINTS=>[[1,-1,-2,-2]]);
 @b1 = map(minkowski_sum($s1, $_), @a);
 @a1 = ();
@@ -181,3 +183,128 @@ foreach my $p (@a5){
 @pts = map(@$_, @pts);
 $pts = new Set<Vector<Integer>>(@pts);
 print $pts->size;
+
+
+# Fourth example
+# Pentagon
+$c = zero_vector(1);
+$b = new Vector([11]);
+$pi = rk3_build_pi(1,1,1,1,1,$c, $b);
+print $pi;
+$tempting = rk3_build_temptings(1,1,1,1,1);
+@a = intersection_approach($tempting, transpose(new Matrix<Rational>($pi)));
+foreach my $a (@a){
+   print "Dim: ", $a->DIM,"\n";
+   print "LinDim: ", $a->LINEALITY_DIM,"\n";
+   print "Vert:\n",$a->VERTICES;
+   print "Lineality:\n", $a->LINEALITY_SPACE;
+   print "------------------------------------------------\n";
+}
+@bounded = grep($_->BOUNDED, @a);
+@lines = grep(!$_->BOUNDED, @a);
+@lpts = map($_->LATTICE_POINTS, @bounded);
+@lpts = map(@$_, @lpts);
+$tmp = new Set<Vector<Integer>>(@lpts);
+print $tmp->size;
+@lpts = @$tmp;
+foreach my $line (@lines){
+   @lpts = grep(!$line->contains($_), @lpts);
+}
+$lpts = new Matrix(@lpts);
+print $lpts;
+$canonical = ones_matrix(1,$lpts->rows) * $lpts;
+$canonical = (new Rational(2,($lpts->rows)))*$canonical;
+print $canonical;
+
+# Fifth example
+# Innocent vector
+$c = zero_vector(1);
+$b = new Vector([11]);
+$pi = rk3_build_pi(1,1,1,1,2,$c, $b);
+print $pi;
+$tempting = rk3_build_temptings(1,1,1,1,2);
+@a = intersection_approach($tempting, transpose(new Matrix<Rational>($pi)));
+foreach my $a (@a){
+   print "Dim: ", $a->DIM,"\n";
+   print "LinDim: ", $a->LINEALITY_DIM,"\n";
+   print "Vert:\n",$a->VERTICES;
+   print "Lineality:\n", $a->LINEALITY_SPACE;
+   print "------------------------------------------------\n";
+}
+@bounded = grep($_->BOUNDED, @a);
+@lines = grep(!$_->BOUNDED, @a);
+@lpts = map($_->LATTICE_POINTS, @bounded);
+@lpts = map(@$_, @lpts);
+$tmp = new Set<Vector<Integer>>(@lpts);
+print $tmp->size;
+@lpts = @$tmp;
+foreach my $line (@lines){
+   @lpts = grep(!$line->contains($_), @lpts);
+}
+$lpts = new Matrix(@lpts);
+print $lpts;
+$canonical = ones_matrix(1,$lpts->rows) * $lpts;
+$canonical = (new Rational(2,($lpts->rows)))*$canonical;
+print $canonical;
+
+# Sixth example
+# Guilty vector
+$c = new Vector([0,9]);
+$b = new Vector([9]);
+$pi = rk3_build_pi(1,1,2,1,1,$c, $b);
+print $pi;
+$tempting = rk3_build_temptings(1,1,2,1,1);
+@a = intersection_approach($tempting, transpose(new Matrix<Rational>($pi)));
+foreach my $a (@a){
+   print "Dim: ", $a->DIM,"\n";
+   print "LinDim: ", $a->LINEALITY_DIM,"\n";
+   print "Vert:\n",$a->VERTICES;
+   print "Lineality:\n", $a->LINEALITY_SPACE;
+   print "------------------------------------------------\n";
+}
+@bounded = grep($_->BOUNDED, @a);
+@lines = grep(!$_->BOUNDED, @a);
+@lpts = map($_->LATTICE_POINTS, @bounded);
+@lpts = map(@$_, @lpts);
+$tmp = new Set<Vector<Integer>>(@lpts);
+print $tmp->size;
+@lpts = @$tmp;
+foreach my $line (@lines){
+   @lpts = grep(!$line->contains($_), @lpts);
+}
+$lpts = new Matrix(@lpts);
+print $lpts;
+$canonical = ones_matrix(1,$lpts->rows) * $lpts;
+$canonical = (new Rational(2,($lpts->rows)))*$canonical;
+print $canonical;
+
+# Sixth example
+# Two Guilty vectors
+$c = new Vector([0,9]);
+$b = new Vector([0,9]);
+$pi = rk3_build_pi(1,1,2,2,1,$c, $b);
+print $pi;
+$tempting = rk3_build_temptings(1,1,2,2,1);
+@a = intersection_approach($tempting, transpose(new Matrix<Rational>($pi)));
+foreach my $a (@a){
+   print "Dim: ", $a->DIM,"\n";
+   print "LinDim: ", $a->LINEALITY_DIM,"\n";
+   print "Vert:\n",$a->VERTICES;
+   print "Lineality:\n", $a->LINEALITY_SPACE;
+   print "------------------------------------------------\n";
+}
+@bounded = grep($_->BOUNDED, @a);
+@lines = grep(!$_->BOUNDED, @a);
+@lpts = map($_->LATTICE_POINTS, @bounded);
+@lpts = map(@$_, @lpts);
+$tmp = new Set<Vector<Integer>>(@lpts);
+print $tmp->size;
+@lpts = @$tmp;
+foreach my $line (@lines){
+   @lpts = grep(!$line->contains($_), @lpts);
+}
+$lpts = new Matrix(@lpts);
+print $lpts;
+$canonical = ones_matrix(1,$lpts->rows) * $lpts;
+$canonical = (new Rational(2,($lpts->rows)))*$canonical;
+print $canonical;
