777c9b601e3e4dd1acfcce71b295ec504ed0b8ea
Anna-Lena Winz <anna-lena.winz@fu-berlin.de>
2017-08-14 14:54:52 +0200

    investigating nuns in picrk3

--- scripts/isolated_rk3.cmd
+++ scripts/isolated_rk3.cmd
@@ -2,7 +2,7 @@ $p0=1;
 $p1=1;
 $c = zero_vector(1);
 $b = zero_vector(2);
-$p4 = 2;
+$p4 = 1;
 $p2 = $c->dim;
 $p3 = $b->dim;
 # canonical for b and c = 0
@@ -23,15 +23,46 @@ foreach my $line (@lines){
 $lpts = new Matrix(@lpts);
 print $lpts->rows;
 print $lpts;
-print $K00;
 
+
+
+
+
+
+
+
+$p0=1;
+$p1=2;
+$c = new Vector([0]);
+$b = new Vector([11]);
+$p4 = 1;
+$p2 = $c->dim;
+$p3 = $b->dim;
+# canonical for b and c = 0
+$K00 = new Vector([-$p2-$p3, $p1-$p3-$p4, -$p0-$p4]);
+$pi = rk3_build_pi($p0,$p1,$p2,$p3,$p4,$c, $b);
+$tempting = rk3_build_temptings($p0,$p1,$p2,$p3,$p4);
+@a = intersection_approach($tempting, transpose(new Matrix<Rational>($pi)));
+@bounded = grep($_->BOUNDED, @a);
+@lpts = map($_->LATTICE_POINTS, @bounded);
+@lpts = map(@$_, @lpts);
+$tmp = new Set<Vector<Integer>>(@lpts);
+print $tmp->size;
+@lpts = @$tmp;
+@lines = grep(!$_->BOUNDED, @a);
+foreach my $line (@lines){
+   @lpts = grep(!$line->contains($_), @lpts);
+}
+$lpts = new Matrix(@lpts);
+print $lpts->rows;
+print $lpts;
 $A1 = $lpts;
-# Make some more...
-#
+$A2 = $lpts;
 $S1 = new Set<Vector<Integer>>(@$A1);
 $S2 = new Set<Vector<Integer>>(@$A2);
-$S3 = new Set<Vector<Integer>>(@$A3);
 $S12 = $S1 * $S2;
+print new Matrix($S12);
+$S3 = new Set<Vector<Integer>>(@$A3);
 $S13 = $S1 * $S3;
 $S23 = $S2 * $S3;
 print $S12 == $S23;
